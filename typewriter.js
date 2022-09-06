333a02b716476328f2a743acd2460062b474425b
MackyClap <martimac2320@gmail.com>
2022-09-05 22:17:12 -0400

    completed typewrite assignment

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,12 @@
 const sentence = "hello there from lighthouse labs";
+
+for (let x = 0; x < sentence.length; x++) {
+  function time () {
+    setTimeout(() => {
+    process.stdout.write(sentence[x])
+  },50 * x)}
+time(x);
+}
+setTimeout(() => {
+  console.log(`\n`);
+}, 2000)
