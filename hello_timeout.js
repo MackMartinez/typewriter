08c14d6d9dde8f34f48eb58982d83aabf9cb7bea
MackyClap <martimac2320@gmail.com>
2022-09-05 21:44:00 -0400

    first commit

---
+++ hello_timeout.js
@@ -0,0 +1,7 @@
+console.log("w0rld")
+setTimeout(()=>{
+  console.log("43110");
+}, 1000);
+setTimeout(()=>{
+  console.log("th3r3");
+}, 2000);
---
+++ matrix_transposition.js
@@ -0,0 +1,44 @@
+const transpose = function (matrix) {
+  let result = [];
+  for (let x = 0; x < matrix[0].length; x++) {
+    result.push([]);
+    for (let y = 0; y < matrix.length; y++) {
+      result[x].push(matrix[y][x]);
+    }
+  }
+  return result;
+};
+
+// Do not edit this function.
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write("\n");
+  }
+};
+
+// code testing area
+printMatrix(
+  transpose([
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+  ])
+);
+
+console.log("----");
+
+printMatrix(
+  transpose([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+);
+
+console.log("----");
+
+printMatrix(transpose([[1, 2, 3, 4, 5, 6, 7]]));
